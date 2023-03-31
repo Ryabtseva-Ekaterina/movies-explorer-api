cccd75589dc9ae4675fe8c0ebd4d267b33d4da2d
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-31 14:03:40 +0300

    corsOptions

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const { errors } = require('celebrate');
+const corsOption = require('./middleware/corsOption');
 const router = require('./routes/index');
 const errorsHandler = require('./middleware/errorsHandler');
-// const corsOption = require('./middleware/corsOption');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 
 const app = express();
-app.use(cors(), (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.status(200);
-    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    res.end();
-    console.log('hello, options');
-  }
-  next();
-});
 app.use(express.json());
 
 mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost:27017/moviesdb', {
@@ -26,6 +16,8 @@ mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI
 });
 
 app.use(requestLogger);
+
+app.use(corsOption);
 
 app.use(router);
 
