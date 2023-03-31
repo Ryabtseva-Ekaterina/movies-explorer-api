4ce1c49fd9d6ea127adcabae07728091a54b1db6
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-31 11:33:02 +0300

    cors option

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const errorsHandler = require('./middleware/errorsHandler');
+const corsOption = require('./middleware/corsOption');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
 
 mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost:27017/moviesdb', {
---
+++ middleware/corsOption.js
@@ -0,0 +1,20 @@
+const allowedCORS = ['*'];
+const METHODS = 'GET, OPTIONS, PATCH, DELETE, POST, PUT';
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeader = req.headers['access-control-request-headers'];
+  res.headers('Access-Control-Allow-Credentials', false);
+
+  if (allowedCORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeader);
+    res.end();
+  }
+  next();
+};
