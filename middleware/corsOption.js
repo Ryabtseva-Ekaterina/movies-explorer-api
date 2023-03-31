4c9f6036c6b3312008937392d3df2af19547abd7
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2023-03-31 12:52:33 +0300

    corsOptions

--- middleware/corsOption.js
+++ middleware/corsOption.js
@@ -1,20 +1,16 @@
-const allowedCORS = '*';
 const METHODS = 'GET, OPTIONS, PATCH, DELETE, POST, PUT';
+const requestHeader = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version';
 
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeader = req.headers['access-control-request-headers'];
-  res.headers('Access-Control-Allow-Credentials', false);
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'false');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', METHODS);
+  res.setHeader('Access-Control-Allow-Headers', requestHeader);
 
-  if (allowedCORS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
+    res.status(200);
     res.header('Access-Control-Allow-Methods', METHODS);
     res.header('Access-Control-Allow-Headers', requestHeader);
     res.end();
   }
-  next();
 };
